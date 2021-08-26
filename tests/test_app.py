fdbdf92bcb659860a053b0c46dabb1782c381ed8
RonnyZurita03 <88996837+RonnyZurita03@users.noreply.github.com>
2021-08-25 21:39:55 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,12 @@
-from app import get_price
+# from app import catfact
 
+# def test_catfact():
+#     fact = catfact().json
+#     print(fact)
 
-def test_get_price():
-    cocacola_price = get_price('KO').json
-    print(cocacola_price)
+#     assert fact['length'] > 0
 
-    assert cocacola_price['price'] > 0
-    assert cocacola_price['name'] == 'The Coca-Cola Company'
-    assert cocacola_price['exchange'] == 'NYSE'
-    assert cocacola_price['currency'] == 'USD'
+def test_catfact():
+    pass
 
-    assert get_price('KSLAFSADF').status_code == 404
-
-
-test_get_price()
+test_catfact()
